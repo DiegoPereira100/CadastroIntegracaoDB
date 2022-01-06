7a67f4c09149efd2bac14fee3f504c0a1bbf4f6b
diego <diegopgcds10@gmail.com>
2022-01-06 17:27:48 -0300

    Configuração do Projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+
+  return (
+
+    <div className={`
+    flex h-screen justify-center items-center
+    bg-gradient-to-r from-purple-500 via-yellow-500 to-blue-600
+
+    `}>
+      <span>Text</span>
+    </div>  
+
+  )
+
+}
